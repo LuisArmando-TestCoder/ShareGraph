7c0da88670f24a4c5170f0fd2f43bb14ef2cf836
Sebas <gomowas@gmail.com>
2021-07-27 18:56:11 -0600

    Se agrego del actions user(Create, desactivate, consult, edit)

--- src/entities/user/actions/consult.py
+++ src/entities/user/actions/consult.py
@@ -1,2 +1,20 @@
+from utilities.getBasicValidInput import getBasicValidInput
+from utilities.getSelectedOption import getSelectedOption
+
 def main():
-    print("#")
+    
+    print("Bienvenido al soporte de usuario que desea saber?")
+    print("1. Crear cuenta 2. Desactivar cuenta 3. Editar cuenta")
+    print("De las opciones anteriores escriba el numero de la que quiere consultar")
+    
+    consult_select = input()
+ 
+    if consult_select == "1":
+        print("Has seleccionado consulta para crear cuentaAl crear la cuenta tendras que escribir tu nombre y apellidos tambien tendras que escribir tu correo y la provincia en la que vives")
+    elif consult_select == "2":
+        print("Has seleccionado Desactivar cuenta al desactivar la cuenta podras borrarar los datos que almacenaste.")
+    
+    elif consult_select == "3":
+        print("Has seleccionado editar cuenta la opcion editar cuenta te ofrece cambiar los datos que pusiste al crear la cuenta.")
+    else: 
+        print("Oops, ocurrio un error, por favor ingresa el numero correcto para elegir")
--- src/entities/user/actions/edit.py
+++ src/entities/user/actions/edit.py
@@ -1,2 +1,27 @@
+from utilities.addDictionaryToFileStore import addDictionaryToFileStore
 def main():
-    print("#")
+    change = input("1. Name 2. Lastname 3. email 4. province 5. password")
+try:
+    if change == "1":
+        name_2 = input("Introduzca el nuevo nombre: ")
+        print("El nuevo nombre es: ", name_2)
+    elif change == "2":
+        lastname_2 = input("Introduzca los nuevos apellidos: ")
+        print("Los nuevos apellidos son: ", lastname_2)
+    elif change == "3":
+        email_2 = input("Introduzca el nuevo correo: ")
+        print("El nuevo correo es :",email_2)
+    elif change == "4":
+        province_2 = input("Introduzca la nueva provincia: ")
+        print("La nueva provincia en la que vive es: ",province_2)
+    elif change == "5":
+        password_2 = input("Introduzca la nueva contraseña: ")
+        while True:
+            confirmepassword_2 = input("Escriba la nueva contraseña para verificar: ")
+            if confirmepassword_2 == password_2:
+                print("Felicidades, la contraseña se cambio.")
+                break
+            else:
+                print("Oops intentelo de nuevo.")
+except ValueError:
+    print("Hubo un error, debe de elegir un numero del 1 al 5 para continuar")    
